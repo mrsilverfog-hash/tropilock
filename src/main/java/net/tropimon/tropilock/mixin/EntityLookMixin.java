0f530285d453d0a537e4186310adb1d961d269a9
mrsilverfog-hash <mrsilverfog@gmail.com>
2026-08-29 19:34:17 +0200

    Update EntityLookMixin.java

--- src/main/java/net/tropimon/tropilock/mixin/EntityLookMixin.java
+++ src/main/java/net/tropimon/tropilock/mixin/EntityLookMixin.java
@@ -1,6 +1,7 @@
 package net.tropimon.tropilock.mixin;
 
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.entity.Entity;
 import net.tropimon.tropilock.TropiLock;
 import org.spongepowered.asm.mixin.Mixin;
@@ -12,7 +13,17 @@ public abstract class EntityLookMixin {
 
     @ModifyVariable(method = "changeLookDirection", at = @At("HEAD"), argsOnly = true, ordinal = 0)
     private double tropilock$blockYawInput(double cursorDeltaX) {
-        if (TropiLock.locked && (Object) this == MinecraftClient.getInstance().player) {
+        if (!TropiLock.locked) {
+            return cursorDeltaX;
+        }
+
+        ClientPlayerEntity player = MinecraftClient.getInstance().player;
+        if (player == null) {
+            return cursorDeltaX;
+        }
+
+        Object self = this;
+        if (self == player || self == player.getRootVehicle() || self == player.getVehicle()) {
             return 0.0;
         }
         return cursorDeltaX;
